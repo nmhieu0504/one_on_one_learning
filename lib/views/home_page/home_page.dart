cbf14932e82ee61f806e8750ebc9c625cc2c96ae
nmhieu0504 <84891428+nmhieu0504@users.noreply.github.com>
2023-04-19 22:54:23 +0700

    Add filter

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -6,9 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:one_on_one_learning/utils/share_pref.dart';
 import 'package:one_on_one_learning/views/home_page/tutor_card_component.dart';
 import 'package:one_on_one_learning/views/meeting_page/meeting_page.dart';
-import 'package:one_on_one_learning/views/tutor_page/tutors_list_page.dart';
 import 'package:http/http.dart' as http;
 import '../../utils/backend.dart';
+import '../../utils/ui_data.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,6 +18,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _isFilter = false;
   bool _loading = true;
   dynamic _data;
 
@@ -38,94 +39,122 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  // List<Widget> _showTutorList() {
-  //   List<Widget> list = [];
-  //   for (int i = 0; i < 10; i++) {
-  //     list.add();
-  //   }
-  //   return list;
-  // }
-
   @override
   void initState() {
     super.initState();
     _loadTutorList();
   }
 
+  Widget _buildFilter() {
+    return SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+      const Text("Tutor Name"),
+      Container(
+        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: SizedBox(        
+          height: 40,
+          child: TextField(
+            textAlignVertical: TextAlignVertical.bottom,
+            cursorColor: Colors.grey,
+            decoration: InputDecoration(
+                fillColor: Colors.grey[200],
+                filled: true,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none),
+                hintText: 'Search',
+                hintStyle: const TextStyle(color: Colors.grey, fontSize: 16),
+                prefixIcon: Container(
+                  padding: const EdgeInsets.all(15),
+                  width: 18,
+                  child: Image.asset(UIData.searchIcon),
+                )),
+          ),
+        ),
+      ),
+    ]));
+  }
+
+  Widget _buildTutorList() {
+    return ListView(children: <Widget>[
+      Container(
+        color: Colors.purple[900],
+        child: Center(
+          child: SizedBox(
+            height: 300,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: const Text(
+                      'Welcome to LetLearn!',
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                  ),
+                  FilledButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (BuildContext context) {
+                            return const MeetingPage();
+                          }),
+                        );
+                      },
+                      child: const Text(
+                        'Book a lesson',
+                      ))
+                ]),
+          ),
+        ),
+      ),
+      Container(
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(top: 10, bottom: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const Text(
+              'Recommended Tutors',
+              style:
+                  TextStyle(fontSize: 18, decoration: TextDecoration.underline),
+            ),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    _isFilter = true;
+                  });
+                },
+                child: const Text('Filters >'))
+          ],
+        ),
+      ),
+      ListView.builder(
+          shrinkWrap: true,
+          physics: const ScrollPhysics(),
+          itemBuilder: (context, index) {
+            return TutorCard(
+              userId: _data["tutors"]["rows"][index]["userId"],
+              avatar: _data["tutors"]["rows"][index]["avatar"],
+              name: _data["tutors"]["rows"][index]["name"],
+              country: _data["tutors"]["rows"][index]["country"],
+              rating: _data["tutors"]["rows"][index]["rating"]?.toInt(),
+              specialties: _data["tutors"]["rows"][index]["specialties"],
+              bio: _data["tutors"]["rows"][index]["bio"],
+            );
+          },
+          itemCount: 10),
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return _loading
         ? const Center(child: CircularProgressIndicator())
-        : ListView(children: <Widget>[
-            Container(
-              color: Colors.purple[900],
-              child: Center(
-                child: SizedBox(
-                  height: 300,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.only(bottom: 10),
-                          child: const Text(
-                            'Welcome to LetLearn!',
-                            style: TextStyle(fontSize: 30, color: Colors.white),
-                          ),
-                        ),
-                        FilledButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (BuildContext context) {
-                                  return const MeetingPage();
-                                }),
-                              );
-                            },
-                            child: const Text(
-                              'Book a lesson',
-                            ))
-                      ]),
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(top: 10, bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  const Text(
-                    'Recommended Tutors',
-                    style: TextStyle(
-                        fontSize: 18, decoration: TextDecoration.underline),
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (BuildContext context) {
-                          return const TutorsListPage();
-                        }));
-                      },
-                      child: const Text('See all >'))
-                ],
-              ),
-            ),
-            ListView.builder(
-                shrinkWrap: true,
-                physics: const ScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return TutorCard(
-                    userId: _data["tutors"]["rows"][index]["userId"],
-                    avatar: _data["tutors"]["rows"][index]["avatar"],
-                    name: _data["tutors"]["rows"][index]["name"],
-                    country: _data["tutors"]["rows"][index]["country"],
-                    rating: _data["tutors"]["rows"][index]["rating"]?.toInt(),
-                    specialties: _data["tutors"]["rows"][index]["specialties"],
-                    bio: _data["tutors"]["rows"][index]["bio"],
-                  );
-                },
-                itemCount: 10),
-          ]);
+        : _isFilter
+            ? _buildFilter()
+            : _buildTutorList();
   }
 }
